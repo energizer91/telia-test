e59a37977f850797951e6b3693fa8496feb5d11e
Alexander Bareyko <militaryboy@yandex.ru>
2021-04-18 21:05:58 +0200

    Initial commit

---
+++ components/MainApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainApp = () => {
+  return <main>imma main app</main>;
+}
+
+export default MainApp;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "script.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      inject: "body"
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devtool: "source-map"
+}
